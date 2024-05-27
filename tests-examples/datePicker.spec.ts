6f14afd969ee53ffcc12620827e7a0ff7d5731de
AnastasiaKharatova <136122979+AnastasiaKharatova@users.noreply.github.com>
2024-05-26 21:10:09 -0400

    add

--- tests-examples/datePicker.spec.ts
+++ tests-examples/datePicker.spec.ts
@@ -2,6 +2,6 @@ import {test as it} from '@playwright/test';
 import {DatePicker} from '../../playwrightAutomationProject/page_object/DatePicker';
 it.describe('DATE PICKER', () => {
    it('working with date picker', async({page}) => {
-    let datePicker()
+    //let datePicker()
    })
 });
