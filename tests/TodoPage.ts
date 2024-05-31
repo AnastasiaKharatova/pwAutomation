d9efd28a59549001ab6b9e250a42ebbf2cb31730
AnastasiaKharatova <136122979+AnastasiaKharatova@users.noreply.github.com>
2024-05-30 23:31:33 -0400

    add

---
+++ tests/TodoPage.ts
@@ -0,0 +1,37 @@
+import { Page } from "@playwright/test";
+
+export class TodoPage{
+    readonly page: Page;
+
+    //** @param {import ('@playwright/test').Page} page */
+    constructor(page:Page) {
+        this.page = page;
+    }
+    get input(){
+        return 'input.new-todo'
+    }
+    get todoItem(){
+        return 'todo-item'
+    }
+
+    async goto(){
+        await this.page.goto('https://demo.playwrigth.dev/todomvc')
+    }
+
+    async addTodo(text: string): Promise<void>{    
+        await this.page.locator(this.input).fill(text)
+        await this.page.keyboard.press('Enter')
+    }
+
+    async remove(text: string): Promise<void>{
+        const todo = this.page.getByTestId(this.todoItem).filter({hasText: text})
+        await todo.hover()
+        await todo.getByLabel('Delete').click()
+    }
+    async removeAll(text: string): Promise<void>{
+        while(await this.page.getByTestId(this.todoItem).count() > 0){
+            this.page.getByTestId(this.todoItem).first().hover()
+            this.page.getByTestId(this.todoItem).first().getByLabel('Delete').click()
+        }
+    }
+}
